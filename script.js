39a366f7bab43d78f3911693fdc973c78712e955
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-03-31 01:16:32 -0300

    Iniciando linktree

---
+++ script.js
@@ -0,0 +1,21 @@
+const btnUm = document.getElementById('btnUm')
+
+btnUm.addEventListener('click', () => {
+    window.location.href = "https://github.com/adrieeltorres1"
+
+})
+
+const btnDois = document.getElementById('btnDois')
+
+btnDois.addEventListener('click', () => {
+    window.location.href = "https://www.linkedin.com/in/adriel-torres-9a421a27b/"
+
+})
+
+
+const btnTres = document.getElementById('btnTres')
+
+btnTres.addEventListener('click', () => {
+    window.location.href = "https://www.linkedin.com/in/adriel-torres-9a421a27b/"
+
+})
