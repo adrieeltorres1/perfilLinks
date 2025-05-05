b49fa20e4499573d323d61e8bd3247ce05197d29
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-05-05 19:36:02 -0300

    Adicionando particulas

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ btnTres.addEventListener('click', () => {
     window.location.href = "https://www.instagram.com/adrieltorres_?igsh=MXZ2bm9nNjB0ZDRneg%3D%3D"
 
 })
+
+// Particulas
+
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".particles-container");
+
+    for (let i = 0; i < 50; i++) { // Número de partículas
+        let particle = document.createElement("div");
+        particle.classList.add("particle");
+
+        particle.style.left = `${Math.random() * 100}vw`;
+        particle.style.animationDuration = `${Math.random() * 5 + 2}s`;
+
+        container.appendChild(particle);
+    }
+});
